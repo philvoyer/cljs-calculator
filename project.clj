4daef793f92821c923062bbc153369d7e408c9e7
philvoyer <philippe.voyer@ift.ulaval.ca>
2019-07-10 10:47:25 -0400

    project configuration

---
+++ project.clj
@@ -0,0 +1,21 @@
+(defproject cljs-calculator "0.1.0-SNAPSHOT"
+  :description "Minimalistic calculator in ClojureScript"
+  :url "calculator.am6.io"
+  :license {:name "MIT"
+            :url "https://tldrlegal.com/license/mit-license"}
+
+  :min-lein-version "2.7.1"
+
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [org.clojure/clojurescript "1.10.339"]
+                 [reagent "0.8.1"]]
+
+  :source-paths ["src"]
+
+  :aliases {"fig"       ["trampoline" "run" "-m" "figwheel.main"]
+            "fig:build" ["trampoline" "run" "-m" "figwheel.main" "-b" "dev" "-r"]
+            "fig:min"   ["run" "-m" "figwheel.main" "-O" "advanced" "-bo" "dev"]
+            "fig:test"  ["run" "-m" "figwheel.main" "-co" "test.cljs.edn" "-m" cljs-calculator.test-runner]}
+
+  :profiles {:dev {:dependencies [[com.bhauman/figwheel-main "0.1.9"]
+                                  [com.bhauman/rebel-readline-cljs "0.1.4"]]}})
